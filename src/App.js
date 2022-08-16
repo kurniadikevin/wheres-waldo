b522b23d8cb5751ebf3ac16ffd425072af0d9ee0
kurniadikevin <kurniadikevin@gmail.com>
2022-08-16 17:29:50 +0700

    add character image

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import Image1 from "./image-1";
 function App() {
   return (
     <div className="App">
-      <div className="sidebar">
-      sidebar
+
+      <div className="topbar">Where is waldo??</div>
+
+      <div className="main">
+         
+          
+          < Image1/>
+         
       </div>
-      <div className="mainbar">
-        <div className="topbar">Where is waldo??</div>
-          < Image1 />
-      </div>
+
     </div>
   );
 }
--- src/image-1.js
+++ src/image-1.js
@@ -4,12 +4,20 @@ const Image1 = () => {
 
 
     return(
+    <div  className="mainbar">
+        <div className="sidebar">
+            <img src={require('./assets/waldo.png')}  alt='waldo' height={100} width={110}/>
+            <img src={require('./assets/odlaw.jpg')}  alt='odwal' height={100} width={100}/>
+            <img src={require('./assets/wenda.png')}  alt='wenda' height={200} width={100}/>
+            </div>
         <div>
+        <div className="img-cont"> 
             <img src={require('./assets/waldo-space.jpg')} alt='waldo-space' 
-            height={933} width={1400}/>
-           
+            height={933} width={1400} className='main-img'/>
+        </div>    
             
         </div>
+    </div>
     )
 }
 
