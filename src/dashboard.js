afbdaeed63cde36f7fd959af89ea999e3f0e8d1a
kurniadikevin <kurniadikevin@gmail.com>
2022-08-18 17:03:28 +0700

    added timer and sample game conditional

--- src/dashboard.js
+++ src/dashboard.js
@@ -1,9 +1,10 @@
+import Timer from "./timer";
 
 const Dashboard = ()=> {
 
     return(
         <div className="dashboard">
-            <div className="timer">Timer</div>
+            <div className="timer-cont"><Timer /></div>
             <div className="head-title">Where's Waldo ?</div>
             <div className="account-area">Account Area</div>
         </div>
--- src/image-1.js
+++ src/image-1.js
@@ -2,10 +2,17 @@ import { useState } from 'react';
 
 const Image1 = () => {
 
-   const {state,setState} = useState([]); 
+const [count,setCount] = useState(3); 
 
+// test conditional
+if( count === 0){
+    alert('game done!')
+}
+// make timer stop when count = 0 and resulting time
+
+
+// sidebar desc
 const displayDesc = (num) => {
-    //alert(ev.target.alt);
     const altText = document.querySelector(`.display-char${num}`);
     altText.style.display='inline';
 }
@@ -14,7 +21,7 @@ const removeDesc = (num) =>{
     const altText = document.querySelector(`.display-char${num}`);
     altText.style.display='none';
 }
-
+// spotted dropdown
 const displayDropdown = (num)=> {
     const dropDown = document.querySelector(`.dropdown-cont${num}`);
     dropDown.style.display='block';
@@ -37,21 +44,30 @@ const removeGetCursor = ()=>{
     const dropDown = document.querySelector('.dropdown-cont0');
     dropDown.style.display='none';
 }
+// spotted character
+const spotted = ()=>{
+    setCount((prev)=> prev - 1);
+    console.log(count);
+}
 
-
+const removeChar = (num)=> {
+    const sideChar = document.querySelector(`#img${num}`);
+    sideChar.style.display='none';
+    alert('remove!!')
+}
     return(
     <div  className="main-bar">
 
         <div className="sidebar">
-            <img src={require('./assets/waldo.png')}  alt='waldo' height={50} width={55} className='side-img' 
+            <img src={require('./assets/waldo.png')}  alt='waldo' height={50} width={55} className='side-img' id='img1'
                 onMouseOver={()=> displayDesc(1)} onMouseLeave={()=>removeDesc(1)}/>
             <p className='display-char1'>Waldo</p>
 
-            <img src={require('./assets/odlaw.jpg')}  alt='odwal' height={50} width={50} className='side-img'
+            <img src={require('./assets/odlaw.jpg')}  alt='odwal' height={50} width={50} className='side-img' id='img2'
              onMouseOver={()=>displayDesc(2)} onMouseLeave={()=>removeDesc(2)}/>
               <p className='display-char2'>Odwal</p>
 
-            <img src={require('./assets/wenda.png')}  alt='wenda' height={110} width={50} className='side-img'
+            <img src={require('./assets/wenda.png')}  alt='wenda' height={110} width={50} className='side-img' id='img3'
              onMouseOver={()=>displayDesc(3)} onMouseLeave={()=>removeDesc(3)}/>
              <p className='display-char3'>Wenda</p>
             </div>
@@ -62,14 +78,15 @@ const removeGetCursor = ()=>{
                 height={933} width={1400} className='main-img'
                 onClick={(ev)=>getCursorPositionDropDown(ev)} onMouseMove={removeGetCursor}/>
             <div className='dropdown-cont0'>
-                    <div >Waldo</div>
+                    <div>Waldo</div>
                     <div>Odlaw</div>
                     <div>Wenda</div>
                 </div>
 
             <div id='spot-waldo' onClick={()=> displayDropdown(1)} onMouseLeave={()=> removeDropdown(1)}>
                 <div className='dropdown-cont1'>
-                    <div >Waldo</div>
+                    <div onClick={()=>{ spotted(); removeChar(1)}}>
+                        Waldo</div>
                     <div>Odlaw</div>
                     <div>Wenda</div>
                 </div>
@@ -77,7 +94,7 @@ const removeGetCursor = ()=>{
             <div id='spot-odlaw'onClick={()=> displayDropdown(2)} onMouseLeave={()=> removeDropdown(2)}>
             <div className='dropdown-cont2'>
                     <div>Waldo</div>
-                    <div>Odlaw</div>
+                    <div onClick={()=>{ spotted(); removeChar(2)}}>Odlaw</div>
                     <div>Wenda</div>
                 </div>
             </div>
@@ -85,7 +102,7 @@ const removeGetCursor = ()=>{
             <div className='dropdown-cont3'>
                     <div>Waldo</div>
                     <div>Odlaw</div>
-                    <div>Wenda</div>
+                    <div onClick={()=>{ spotted(); removeChar(3)}}>Wenda</div>
                 </div>
             </div>
         </div>    
